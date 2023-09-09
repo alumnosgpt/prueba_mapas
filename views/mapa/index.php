340daf9b689b3dfde4e888e8585ded039ab4ab22
lestermaudiel <flestermaudiel2130@gmail.com>
2023-09-08 23:27:51 -0600

    se actualizo el boton de actualizar.

--- views/mapa/index.php
+++ views/mapa/index.php
@@ -1,9 +1,10 @@
 <h1 class="text-center">Prueba mapas</h1>
 <div class="row justify-content-center mb-3">
-    
-<button class="btn btn-danger btn-sm" id="actualizar" name="actualizar">ACTUALIZAR</button>
-    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto;">
+    <form>
+        <button class="btn btn-danger btn-sm" id="actualizar" name="actualizar">ACTUALIZAR</button>
+    </form>
+    <div class="col-lg-11 p-3 border rounded" id="map" style="height: 60vh;min-height:auto;" />
 
-    </div>
+
 </div>
 <script src="<?= asset('build/js/mapa/index.js') ?>"></script>
